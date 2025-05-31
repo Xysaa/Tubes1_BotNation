c3f785e36bf516c10b38c001d28e5b32e0411195
Stevanus Cahya Anggara <stevanuschyaanggara@gmail.com>
2025-05-31 22:55:09 +0700

    BOT GACOR

---
+++ run-bots.bat
@@ -0,0 +1,5 @@
+@echo off
+start cmd /c "python main.py --logic Simple --email=test@email.com --name=Jago --password=123456 --team etimo"
+start cmd /c "python main.py --logic Simple --email=test1@email.com --name=Jago1 --password=123456 --team etimo"
+start cmd /c "python main.py --logic Simple --email=test2@email.com --name=Jagoan --password=123456 --team etimo"
+start cmd /c "python main.py --logic Simple --email=test3@email.com --name=Jagoan2 --password=123456 --team etimo"
